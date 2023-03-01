175a122e992dbc5ce29bb32bff6d1a1c72b350fa
Andres Hincapie <ahincapie@Andress-MacBook-Pro-2.local>
2023-02-28 23:16:45 -0500

    Header done

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,22 +2,28 @@ import { Link } from "react-router-dom";
 
 function Nav(props) {
   return (
-    <div className="nav">
-      <Link to="/">
-        <div>Home</div>
-      </Link>
-      <Link to="/about">
-        <div>About</div>
-      </Link>
-      <Link to="/sports">
-        <div>Sports</div>
-      </Link>
-      <Link to="/business">
-        <div>Business</div>
-      </Link>
-      <Link to="/tech">
-        <div>Technology</div>
-      </Link>
+    <div className="container">
+      <div>
+        <img className="logo col-2" src="2.png" alt="Bostonian Post"/>
+      </div>
+
+      <div className="nav container">
+        <Link to="/">
+          <div>Home</div>
+        </Link>
+        <Link to="/about">
+          <div>About</div>
+        </Link>
+        <Link to="/sports">
+          <div>Sports</div>
+        </Link>
+        <Link to="/business">
+          <div>Business</div>
+        </Link>
+        <Link to="/tech">
+          <div>Technology</div>
+        </Link>
+      </div>
     </div>
   );
 }
